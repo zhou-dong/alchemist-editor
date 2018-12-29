0b44a9a5169c34fce1a61349ee9f4bf7f43588cc
dozhou <dozhou@rubiconproject.com>
2018-12-29 03:36:47 -0800

    set default language to js

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import './App.css';
 import brace from 'brace';
 import AceEditor from 'react-ace';
 
-import 'brace/mode/java';
-import 'brace/theme/github';
+import 'brace/mode/javascript';
+import 'brace/theme/ghub';
 
 function onChange(newValue: any) {
   console.log('change', newValue);
+  // eval(newValue);
 }
 
 const editor = () => {
   return (
     <AceEditor
-      mode="java"
+      mode="javascript"
       theme="github"
       onChange={onChange}
       name="UNIQUE_ID_OF_DIV"
